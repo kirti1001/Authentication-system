91ca595926b3600a69e087068862e070ff220332
kirti1001 <kirti8933924496@gmail.com>
2025-09-04 19:02:50 +0000

    updating database connectivity

--- database/database.js
+++ database/database.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
-const MONGODB_URL = process.env.PORT
+const { MONGODB_URL } = process.env.MONGODB_URL
 exports.connect = () => {
     mongoose.connect(MONGODB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
-    .then()
+    .then(
+        console.log(`DB Connected sucessfully`)
+    )
     .catch((error) => {
         console.log(`DB connection FAILED`);
         console.log(error);
